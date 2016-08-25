b4a9fae54c53c38581c79889dc050cc3873ebee3
austinjp <austin.plunkett@gmail.com>
2016-08-25 15:43:24 +0000

    Oops, taken func out of main entry func

--- index.js
+++ index.js
@@ -21,69 +21,72 @@ b2cj.prototype.parsefile = function(bibfile, lang, localesfile, stylefile) {
 
     var bib = engine.makeBibliography();
 
-    return { bibliography: bib, csljson: csljson };
+    return {
+	bibliography: bib,
+	csljson: csljson
+    };
 
-    // console.log(util.inspect(bib), true,null,true);
+}
+
     
 
-    function jsonToCSLJSON(json) {
-	// This is half-arsed, but it works for my purposes.
-	var cslJson = {};
-	for (var key in json) {
-            if (json.hasOwnProperty(key)) {
-		var obj = json[key];
+function jsonToCSLJSON(json) {
+    // This is half-arsed, but it works for my purposes.
+    var cslJson = {};
+    for (var key in json) {
+        if (json.hasOwnProperty(key)) {
+	    var obj = json[key];
 
-		var ID;
+	    var ID;
 
-		for (k2 in obj) {
-                    if (obj.hasOwnProperty(k2)) {
+	    for (k2 in obj) {
+                if (obj.hasOwnProperty(k2)) {
 
-			if (typeof k2 === "string") {
-                            if (k2.toLowerCase() === "citationkey") {
-				ID = obj[k2];
-				cslJson[ID] = {};
-                            }
-			}
+		    if (typeof k2 === "string") {
+                        if (k2.toLowerCase() === "citationkey") {
+			    ID = obj[k2];
+			    cslJson[ID] = {};
+                        }
+		    }
 
-			if (typeof ID !== "undefined") {
-                            /*
-                              if (k2.toLowerCase() === "entrytype") {
-                              // Ignore for now..?
-                              }
-                            */
-                            if (k2.toLowerCase() === "entrytags") {
-				// Copy the object making all keys lower case.
-				// Means there is far less checking to do.
-				var oldObj = obj[k2];
-				var tags = {};
-				var oldKey, oldKeys = Object.keys(oldObj);
-				var n = oldKeys.length;
-				while (n--) {
-                                    oldKey = oldKeys[n];
-                                    tags[oldKey.toLowerCase()] = oldObj[oldKey];
-				}
+		    if (typeof ID !== "undefined") {
+                        /*
+                          if (k2.toLowerCase() === "entrytype") {
+                          // Ignore for now..?
+                          }
+                        */
+                        if (k2.toLowerCase() === "entrytags") {
+			    // Copy the object making all keys lower case.
+			    // Means there is far less checking to do.
+			    var oldObj = obj[k2];
+			    var tags = {};
+			    var oldKey, oldKeys = Object.keys(oldObj);
+			    var n = oldKeys.length;
+			    while (n--) {
+                                oldKey = oldKeys[n];
+                                tags[oldKey.toLowerCase()] = oldObj[oldKey];
+			    }
 
-				// All keys here must be lower case or a weird error pops out.
-				cslJson[ID]["id"] = ID;
-				cslJson[ID]["url"] = tags.url ? tags.url : undefined;
-				cslJson[ID]["doi"] = tags.doi ? tags.doi : undefined;
-				cslJson[ID]["title"] = tags.title ? tags.title : undefined;
-				cslJson[ID]["page"] = tags.pages ? tags.pages : undefined;
+			    // All keys here must be lower case or a weird error pops out.
+			    cslJson[ID]["id"] = ID;
+			    cslJson[ID]["url"] = tags.url ? tags.url : undefined;
+			    cslJson[ID]["doi"] = tags.doi ? tags.doi : undefined;
+			    cslJson[ID]["title"] = tags.title ? tags.title : undefined;
+			    cslJson[ID]["page"] = tags.pages ? tags.pages : undefined;
 
-				// FIXME Placeholder function, parse date.
-				cslJson[ID]["accessed"] = { "day":"1","month":"9","year":"2012" };
+			    // FIXME Placeholder function, parse date.
+			    cslJson[ID]["accessed"] = { "day":"1","month":"9","year":"2012" };
 
-				// FIXME Placeholder function, parse authors.
-				cslJson[ID]["author"] = [{ "given": Math.random().toString(26).substring(2,7), "family": Math.random().toString(26).substring(2,7) }];
-                            }
-			}
+			    // FIXME Placeholder function, parse authors.
+			    cslJson[ID]["author"] = [{ "given": Math.random().toString(26).substring(2,7), "family": Math.random().toString(26).substring(2,7) }];
+                        }
+		    }
 
-                    }
-		}
-            }
-	}
-	return cslJson;
+                }
+	    }
+        }
     }
+    return cslJson;
 }
 
 exports.b2cj = new b2cj();
